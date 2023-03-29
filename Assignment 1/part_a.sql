c59ba322a8ceda2a243a1aeb01e236becebff849
Matt <5d3si7ps@inboxe.anonaddy.com>
2023-03-30 09:39:40 +1100

    Working on it!

--- Assignment 1/part_a.sql
+++ Assignment 1/part_a.sql
@@ -1,8 +1,8 @@
 /*
-    This is how you write a multi-line comment.
-    Keep all SQL queries and answers related to part A here.
+This is how you write a multi-line comment.
+Keep all SQL queries and answers related to part A here.
 
-    https://www.w3schools.com/sql/
+https://www.w3schools.com/sql/
 */
 
 -- Create the database (remember to run as a 'script', not as a 'statement')
@@ -19,8 +19,7 @@ branch:     branchid, branchname;
 loan:       isbn, branchid, cardno, dateout, datein;
 */
 
-/*
-Question 1
+/* Question 1
 List in ascending order the Last Names of all borrowers with Card Number less than 150.
 */
 
@@ -29,9 +28,10 @@ FROM borrower
 WHERE cardno < 150
 ORDER BY lname ASC;
 
-/*
-Question 2
+
+/* Question 2
 What are the records of those loans between 3 June 2014 and 8 Mar 2018?
+
 - The output should include the name of the cardholder, and title of the book.
 - List in ascending order by last name and first name
 */
@@ -43,9 +43,19 @@ WHERE dateout
 BETWEEN '03/JUN/14' AND '08/MAR/18'
 ORDER BY lname ASC;
 
--- Question 3
 
--- Question 4
+/* Question 3
+Find the total numbers of loans with last name beginning with D through E (including E);include last name beginning with Q as well as Z also.
+
+- The output of the above question should be a column containing the sum of all the loanbased on the condition in the query, alternatively the output could be a table with columnsthat has the last name as per the condition in the query condition, also the table containsthe list of loans.
+*/
+
+
+/* Question 4
+Find all borrowers for a loan that have the Date-in before 15-March-15 and the Card numberbetween 100 and 300 in Karama or Darwin.
+
+- The output for the above question is a list of all borrowers (first name and last name)based on the condition in the query.
+*/
 
 SELECT (fname || ' '|| lname)
 FROM loan
@@ -54,6 +64,15 @@ WHERE borrower.cardno BETWEEN 100 AND 300
     AND loan.datein < '15/MAR/15'
     AND (suburb = 'KARAMA' OR suburb = 'DARWIN');
 
--- Question 5
 
--- Question 6
+/* Question 5
+Find the number of loans, which have been made from each branch?
+
+- Note all branches need to be included even those with no loans
+- The output of the above question is a table containing the list of loans only as per thecondition in the query
+*/
+
+
+/* Question 6
+Create your own question based on the library database, and also provide a SQLStatement to answer your question.
+*/
--- Assignment 1/part_b.sql
+++ Assignment 1/part_b.sql
@@ -4,9 +4,21 @@
 Keep all SQL queries and answers related to part B here.
 */
 
--- Question 1
+/* Question 1
+Question is too long to put here, refer to assignment for instructions.
+This answer is incomplete.
+*/
 
-CREATE TABLE Occurrence_Exercise;
+CREATE TABLE Occurrence_Exercise (
+    OccExID INT PRIMARY KEY,
+    ExerciseNo INT FOREIGN KEY,
+    OccurrenceID INT FOREIGN KEY,
+    Rep INT,
+    Attempt1 INT,
+    Attempt2 INT,
+    Attempt3 INT,
+    Condition VARCHAR(6)
+);
 
 -- Question 2
 
