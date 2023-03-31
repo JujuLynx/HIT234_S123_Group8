e010a7a975524d39889101f5978d875ac3c483e4
KyleEsson <89835575+KyleEsson@users.noreply.github.com>
2023-03-31 10:02:50 +0930

    Update part_c.sql
    attempted 1 method in Q4

--- Assignment 1/part_c.sql
+++ Assignment 1/part_c.sql
@@ -11,5 +11,17 @@ Keep all SQL queries and answers related to part C here.
 -- Question 3
 
 -- Question 4
+/*Find the borrower that has returned a book on the dated 16 December 2010 and 8 May 2012
+◦	Show two different methods.
+*/
+
+--METHOD 1
+
+SELECT (B.FNAME ||' '|| B.LNAME) AS BORROWER_NAME
+FROM BORROWER B, LOAN L1, LOAN L2
+WHERE B.CARDNO = L1.CARDNO
+AND L1.CARDNO = L2.CARDNO
+AND L1.DATEIN = '16-DEC-2010'
+AND L2.DATEIN = '8-MAY-2012';
 
 -- Question 5
