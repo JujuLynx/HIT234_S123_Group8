d14b59070d0c2659c042a1ce31eddc3bc629b61b
Matt <5d3si7ps@inboxe.anonaddy.com>
2023-04-20 15:12:00 +1000

    Minor edits.

--- Assignment 1/part_a.sql
+++ Assignment 1/part_a.sql
@@ -12,6 +12,13 @@ CREATE TABLE book AS SELECT * FROM jtutty.book;
 CREATE TABLE branch AS SELECT * FROM jtutty.branch;
 CREATE TABLE loan AS SELECT * FROM jtutty.loan;
 
+-- Cleanup
+
+DROP TABLE borrower;
+DROP TABLE book;
+DROP TABLE branch;
+DROP TABLE loan;
+
 /* Columns
 borrower:   cardno, lname, fname, suburb, postcode;
 book:       isbn, title, yearpublished, publishername;
